f5ba5f354ce5da40fdfeb6d5f70847705588feec
Damian <arcvaartmans@gmail.com>
2021-01-31 12:19:05 +0100

    meer code toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-alert("hello world");
+var menuItem = document.getElementById('menuItem');
+var menuItemOther = document.getElementById('menuItemOther');
+var menuClickCheck = false;
+
+function MenuClick(){
+	if (menuClickCheck == false) {
+		menuItemOther.style.display = "block";
+		menuClickCheck = true;
+	}
+	else{
+		menuItemOther.style.display = "none";
+		menuClickCheck = false;
+	}
+}
+
+menuItem.onclick = function(){
+	MenuClick();
+}
